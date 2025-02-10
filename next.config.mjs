d4da67628e10385eaac90c6be3f5097f07befda7
danish-noor <kinzadani00@gmail.com>
2025-02-10 19:35:13 +0500

    fix: fixing images

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,39 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "cdn.sanity.io",
+        
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     unoptimized: true, // Local images ke liye optimization disable karein
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "cdn.sanity.io",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
-    ],
+    domains: ["cdn.sanity.io"], // Remote images ke liye
+    unoptimized: true, // Local images ke liye optimization disable
   },
 };
 
 export default nextConfig;
+
