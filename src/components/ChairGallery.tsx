d75e6e03ba2ebbae9a000eb706e867b92825895e
danish-noor <kinzadani00@gmail.com>
2025-02-12 11:09:02 +0500

    fix: images

--- src/components/ChairGallery.tsx
+++ src/components/ChairGallery.tsx
@@ -5,8 +5,8 @@ export default function ChairGallery() {
     <div className="w-full my-8 sm:px-6 md:px-12 lg:px-16 flex flex-col sm:flex-row justify-center items-center gap-3 rounded-md">
       <div className=" sm:w-[45vw] w-screen">
         <Image 
-        src="/images/orange1.png"
-        alt="orange chair1" 
+        src="./orange1.png"
+        alt="orange-chair" 
         height={800} 
         width={800} 
         />
@@ -15,31 +15,28 @@ export default function ChairGallery() {
       <div className="flex w-screen sm:w-[45vw] gap-3">
         <div className="h-1/2 w-1/2 relative flex flex-col gap-3">
           <Image
-            src={"/images/white1.png"}
-            alt="white1"
+            src="/white1.png"
+            alt="white-chair"
             height={400}
             width={400}
           />
           <Image
-            className=""
-            src={"/images/chair2.png"}
-            alt="orange chair3"
+            src="/chair2.png"
+            alt="chair2"
             height={400}
             width={400}
           />
         </div>
         <div className="h-1/2 w-1/2 relative flex flex-col gap-3 ">
           <Image
-            className=""
-            src={"/images/gray1.png"}
-            alt="orange chair4"
+            src="/gray1.png"
+            alt="gray1"
             height={400}
             width={400}
           />
           <Image
-            className=""
-            src={"/images/category2.png"}
-            alt="orange chair5"
+            src="/category2.png"
+            alt="stool chair"
             height={400}
             width={400}
           />
